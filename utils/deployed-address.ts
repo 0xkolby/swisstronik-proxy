06e7afff98f3fbcd02eb37e2277657f2bc9f17ab
0xkolby rohwer <kolbyrohwer@gmail.com>
2024-09-15 09:50:37 +0000

    feat: initiated the project

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xfdE9213d05c7f189e9921e4d0B66b8f92bB8A050'
+export const ProxyAdmin = '0x4467D60649e02871Bc6723398Fd8Ea49A8a22bE4'
+export const SWTRImplementation = '0x6A94B53bCCa4A885f99F3C6f7f0E24C5f3e51348'
+    
